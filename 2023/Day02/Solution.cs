f21207c076bb01b44a29a0d42b541c1d1c1bae3b
Kossuranta <kossuranta@gmail.com>
2023-12-03 20:02:28 +0200

    Solve Day 02 Part 1

---
+++ 2023/Day02/Solution.cs
@@ -0,0 +1,59 @@
+﻿namespace AdventOfCode2023.Day02
+{
+    internal class SolutionPart1
+    {
+        private static readonly Dictionary<string, int> limits = new()
+        {
+            { "red", 12 },
+            { "green", 13 },
+            { "blue", 14 },
+        };
+
+        public static string Solve()
+        {
+            string[] lines = File.ReadLines(@"Day02/input.txt").ToArray();
+            int sum = 0;
+            for(int index = 0; index < lines.Length; index++)
+            {
+                string line = lines[index];
+                int gameIndex = index + 1;
+                if (IsGamePossible(line))
+                    sum += gameIndex;
+            }
+            return sum.ToString();
+        }
+
+        public static bool IsGamePossible(string line)
+        {
+            line = line.Split(':').Last();
+            line = line.Replace(" ", string.Empty);
+            string[] inputs = line.Split(';');
+            foreach (string input in inputs)
+            {
+                string[] cubes = input.Split(",");
+                foreach (string cube in cubes)
+                {
+                    (string color, int amount) = ParseColorAmountPair(cube);
+                    if (amount > limits[color])
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static (string, int) ParseColorAmountPair(string line)
+        {
+            foreach((string color, int _) in limits)
+            {
+                if (line.Contains(color))
+                {
+                    string amount = line.Replace(color, string.Empty);
+                    return (color, int.Parse(amount));
+                }
+            }
+
+            return (string.Empty, 0);
+        }
+    }
+}
--- 2023/Program.cs
+++ 2023/Program.cs
@@ -1,2 +1,3 @@
 ﻿Console.WriteLine($"Day 01 - Part 1: {AdventOfCode2023.Day01.SolutionPart1.Solve()}");
 Console.WriteLine($"Day 01 - Part 2: {AdventOfCode2023.Day01.SolutionPart2.Solve()}");
+Console.WriteLine($"Day 02 - Part 1: {AdventOfCode2023.Day02.SolutionPart1.Solve()}");
