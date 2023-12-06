8deb501dec05c56792472015be24ffba867c7b4c
Kossuranta <kossuranta@gmail.com>
2023-12-06 22:13:04 +0200

    Edit Day 06 code to solve Part 2

--- 2023/Day06/Solution.cs
+++ 2023/Day06/Solution.cs
@@ -3,6 +3,7 @@
     internal class Solution
     {
         private static (int time, int distance)[] races;
+        private static (long time, long distance) race;
 
         public static string Solve()
         {
@@ -10,21 +11,24 @@
             ParseInput(lines);
             int p1 = 1;
             foreach ((int time, int distance) in races)
-            {
-                int waysToWin = 0;
-                for(int holdTime = 1;  holdTime < time; holdTime++)
-                {
-                    int dist = holdTime * (time - holdTime);
-                    if (dist > distance)
-                        waysToWin++;
-                }
-
-                p1 *= waysToWin;
-            }
-
-            int p2 = 0;
+                p1 *= CountWaysToWin(time, distance);
+            
+            int p2 = CountWaysToWin(race.time, race.distance);
             string result = $"Part 1: {p1} | Part 2: {p2}";
             return result;
+        }
+
+        private static int CountWaysToWin(long time, long distance)
+        {
+            int waysToWin = 0;
+            for (int holdTime = 1; holdTime < time; holdTime++)
+            {
+                long dist = holdTime * (time - holdTime);
+                if (dist > distance)
+                    waysToWin++;
+            }
+
+            return waysToWin;
         }
 
         private static void ParseInput(string[] lines)
@@ -32,8 +36,17 @@
             string[] timesStr = lines[0].Split(' ', StringSplitOptions.RemoveEmptyEntries);
             string[] distancesStr = lines[1].Split(' ', StringSplitOptions.RemoveEmptyEntries);
             races = new (int, int)[timesStr.Length - 1];
+            string timeStr = "";
+            string distanceStr = "";
             for(int i = 1; i < timesStr.Length; i++)
+            {
                 races[i - 1] = (int.Parse(timesStr[i]), int.Parse(distancesStr[i]));
+
+                timeStr += timesStr[i];
+                distanceStr += distancesStr[i];
+            }
+
+            race = (long.Parse(timeStr), long.Parse(distanceStr));
         }
     }
 }
